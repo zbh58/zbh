1c0c94b0af80d6f7c0d7f856959f037f03ba7182
heyinlonga <2022501519@qq.com>
2020-12-15 21:26:18 +0800

    杀死大哥

--- app/src/main/java/com/example/zbh/MainActivity.java
+++ app/src/main/java/com/example/zbh/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 		//辣鸡
+		//发撒和发送附件阿三
 		//思密达
     }
 }
